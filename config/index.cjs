a2fb2469ac83feb61acc63798bf1d20893fddd0f
jwanng <87064720+Mandorian@users.noreply.github.com>
2024-05-20 22:19:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,15 +51,15 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'jCfLmz8iWl7shQt-2O3w6MMF7NLZfX-ZiZ_x0LQQFKI',
+  // CALLBACK_TEMPLATE_ID: 'jCfLmz8iWl7shQt-2O3w6MMF7NLZfX-ZiZ_x0LQQFKI',
 
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o3LBk6S6yI-HYvPwvi7dqKlMgjzY',
-    }
-  ],
+  // CALLBACK_USERS: [
+  //   {
+  //     name: '自己',
+  //     // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+  //     id: 'o3LBk6S6yI-HYvPwvi7dqKlMgjzY',
+  //   }
+  // ],
 
 }
 
